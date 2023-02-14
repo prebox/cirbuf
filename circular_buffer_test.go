6ecf8a633a36487b2ab4c779578f01f934258d1f
prebox <prebox@mail.com>
2023-02-14 23:41:48 +0100

    Add test for Dequeue

--- circular_buffer_test.go
+++ circular_buffer_test.go
@@ -43,3 +43,47 @@ func TestEnqueue(t *testing.T) {
 		})
 	}
 }
+
+func TestDequeue(t *testing.T) {
+	testCases := []struct {
+		name     string
+		expected int
+		count    int
+		head     int
+		tail     int
+		err      error
+	}{
+		{"dequeue first", 1, 2, 1, 0, nil},
+		{"dequeue second", 2, 1, 2, 0, nil},
+		{"dequeue third", 3, 0, 0, 0, nil},
+		{"dequeue empty", 0, 0, 0, 0, ErrorIsEmpty},
+	}
+
+	q := New[int](3)
+
+	// Manually set the state of the buffer.
+	q.data = []int{1, 2, 3}
+	q.tail = 0
+	q.count = 3
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			item, err := q.Dequeue()
+			if item != tc.expected {
+				t.Errorf("Expected %v, but got: %v", tc.expected, item)
+			}
+			if q.count != tc.count {
+				t.Errorf("Expected %v, but got: %d", tc.count, q.count)
+			}
+			if q.head != tc.head {
+				t.Errorf("Expected %v, but got: %d", tc.head, q.head)
+			}
+			if q.tail != tc.tail {
+				t.Errorf("Expected %v, but got: %d", tc.tail, q.tail)
+			}
+			if err != tc.err {
+				t.Errorf("Expected %v, but got: %v", tc.err, err)
+			}
+		})
+	}
+}
