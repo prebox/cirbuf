4a4bb55b6f6a72903c127a0fd9dfae294f4003a7
prebox <prebox@mail.com>
2023-02-15 12:28:51 +0100

    Add IsFull test cases

---
+++ testcases/is_full.go
@@ -0,0 +1,15 @@
+package testcases
+
+// Test cases for IsFull.
+var IsFull = []struct {
+	Name   string
+	Data   []int
+	Head   int
+	Tail   int
+	Count  int
+	Expect bool
+}{
+	{Name: "empty buffer", Data: []int{0, 0, 0}, Head: 0, Tail: 0, Count: 0, Expect: false},
+	{Name: "partially full buffer", Data: []int{1, 0, 0}, Head: 0, Tail: 1, Count: 1, Expect: false},
+	{Name: "full buffer", Data: []int{1, 2, 3}, Head: 0, Tail: 0, Count: 3, Expect: true},
+}
