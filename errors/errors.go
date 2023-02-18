59a8eaaf5ec4142b1f65e290fde65403de2f58f7
prebox <prebox@mail.com>
2023-02-18 16:36:36 +0100

    Add error for invalid capacity

--- errors/errors.go
+++ errors/errors.go
@@ -3,7 +3,8 @@ package errors
 import "errors"
 
 var (
-	ErrorIsFull      = errors.New("queue is full")
-	ErrorIsEmpty     = errors.New("queue is empty")
-	ErrorOutOfBounds = errors.New("index out of bounds")
+	ErrorIsFull          = errors.New("queue is full")
+	ErrorIsEmpty         = errors.New("queue is empty")
+	ErrorOutOfBounds     = errors.New("index out of bounds")
+	ErrorInvalidCapacity = errors.New("invalid capacity")
 )
